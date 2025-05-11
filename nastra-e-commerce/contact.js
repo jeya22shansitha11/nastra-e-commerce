81f304fa0d6f5190d75b4e1ad25918d9d5097318
jeya22shansitha11 <jshansitha@gmail.com>
2025-05-11 21:22:13 +0530

    completed the basic pages

---
+++ nastra-e-commerce/contact.js
@@ -0,0 +1,7 @@
+var sub = document.querySelector("sub")
+var Input = document.getElementById("one")
+var Input = document.getElementById("two")
+var Input = document.getElementById("three")
+function clear(){
+    Input.display="none";
+}
---
+++ nastra-e-commerce/index.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector(".side-navbar");
+    function showNavbar()
+    {
+        console.log("fuction called")
+        sidenav.style.left="0";
+    }
+    function closeNavbar(){
+           sidenav.style.left="-60%";
+    }
+
+
